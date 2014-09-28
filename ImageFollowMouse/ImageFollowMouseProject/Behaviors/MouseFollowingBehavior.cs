8f48d609bdf2698d6ab6182f62e23be7c1f84fcd
Chaos Yu <chaos.you@gmail.com>
2014-09-28 11:20:21 +0800

    Add ImageFollowMouse example, not complete

---
+++ ImageFollowMouse/ImageFollowMouseProject/Behaviors/MouseFollowingBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveEngine.Common.Input;
+using WaveEngine.Common.Math;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Services;
+
+namespace ImageFollowMouseProject.Behaviors
+{
+    public class MouseFollowingBehavior : Behavior
+    {
+        [RequiredComponent]
+        private Transform2D _transform = null;
+
+        private Input _input;
+        
+
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            _input = WaveServices.Input;
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            _transform.X = _input.MouseState.X;
+            _transform.Y = _input.MouseState.Y;
+        }
+    }
+}
---
+++ ImageFollowMouse/ImageFollowMouseProject/MyScene.cs
@@ -0,0 +1,43 @@
+#region Using Statements
+using System;
+using WaveEngine.Common;
+using WaveEngine.Common.Graphics;
+using WaveEngine.Common.Math;
+using WaveEngine.Components.Cameras;
+using WaveEngine.Components.Graphics2D;
+using WaveEngine.Components.Graphics3D;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Resources;
+using WaveEngine.Framework.Services;
+#endregion
+
+namespace ImageFollowMouseProject
+{
+    public class MyScene : Scene
+    {
+        protected override void CreateScene()
+        {
+            //Insert your scene definition here.
+
+            var camera = new FixedCamera2D("camera");
+
+            this.EntityManager.Add(camera);
+
+            var img = new Entity()
+                .AddComponent(new Transform2D() { Origin = new Vector2(0.5f, 0.5f) })
+                .AddComponent(new Sprite("Content/ein"))
+                .AddComponent(new SpriteRenderer(DefaultLayers.Opaque))
+                .AddComponent(new Behaviors.MouseFollowingBehavior());
+
+            this.EntityManager.Add(img);
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            // This method is called after the CreateScene and Initialize methods and before the first Update.
+        }
+    }
+}
